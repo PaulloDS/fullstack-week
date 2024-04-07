b87d4fc66d7866519c226c0c279f8b9d15713d7a
PaulloDS <paulosch14@gmail.com>
2024-04-07 02:34:43 -0300

    feat: add Categories and CategoriesItem component

--- pds-store/src/app/(home)/page.tsx
+++ pds-store/src/app/(home)/page.tsx
@@ -1,12 +1,17 @@
 "use client"
 
 import Image from "next/image"
+import Categories from "./components/categories";
 
 export default function Home() {
 
   return (
     <div className="p-5">
       <Image className="w-full h-auto" src='/banner-home-01.png' width={0} height={0} alt="Até 55% de desconto só esse mês" sizes="100vw"/>
+
+      <div className="mt-8">
+          <Categories/>
+      </div>
     </div>
   );
 }
