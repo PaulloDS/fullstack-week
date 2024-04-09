5daab99764511bce52194ebb9650464ec2d42027
PaulloDS <paulosch14@gmail.com>
2024-04-08 22:04:17 -0300

    fix: ensure correct product totalPrice is calculated

--- pds-store/src/helpers/product.ts
+++ pds-store/src/helpers/product.ts
@@ -1,18 +1,22 @@
 import { Product } from "@prisma/client";
-import { Decimal } from "@prisma/client/runtime/library";
 
 export interface ProductWithTotalPrice extends Product {
     totalPrice: number;
 }
 
 export const computeProductTotalPrice = (product: Product): ProductWithTotalPrice => {
-    const basePriceNumber = (product.basePrice as Decimal).toNumber();
-    const totalPrice = product.discountPercentage === 0 ?
-        basePriceNumber :
-        basePriceNumber - (basePriceNumber * product.discountPercentage) / 100;
+    if (product.discountPercentage === 0) {
+        return {
+            ... product,
+            totalPrice: Number(product.basePrice),
+        }
+    }
+
+    const totalDiscount =
+        Number(product.basePrice) * (product.discountPercentage / 100);
 
     return {
-        ...product,
-        totalPrice: Number(totalPrice)
-    };
+        ... product,
+        totalPrice: Number(product.basePrice) - totalDiscount
+    }
 }
