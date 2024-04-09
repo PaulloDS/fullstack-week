2cebedea090aa93050bbac6734b15c963fe67f4d
PaulloDS <paulosch14@gmail.com>
2024-04-08 22:07:35 -0300

    feat: add second banner to home page

--- pds-store/src/app/(home)/page.tsx
+++ pds-store/src/app/(home)/page.tsx
@@ -20,8 +20,11 @@ export default async function Home() {
           <Categories/>
       </div>
       <div className="mt-8">
+        <p className="font-bold uppercase pl-5 mb-3">Ofertas</p>
         <ProductList products={deals}/>
       </div>
+
+      <Image className="w-full h-auto px-5 mt-8" src='/banner-home-02.png' width={0} height={0} alt="Até 55% de desconto em mouses!" sizes="100vw"/>
     </div>
   );
 }
