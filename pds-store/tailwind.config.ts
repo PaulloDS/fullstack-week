f61332ab8bd144d88b7bead72bbea5da1b3283e1
PaulloDS <paulosch14@gmail.com>
2024-04-10 21:22:00 -0300

    chore: add category-item-gradient to tailwind config

--- pds-store/tailwind.config.ts
+++ pds-store/tailwind.config.ts
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+      backgroundImage: {
+        "category-item-gradient": "linear-gradient(45deg, #5033c3 0%, rgba(80, 51, 195, 0.20) 100%);"
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
